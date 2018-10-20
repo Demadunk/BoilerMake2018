5681d6a27292f9b56575544dbb57a8c3a971889b
lu656 <lu656@purdue.edu>
2018-10-20 10:20:54 -0400

    changed style from true/false to individual animal counters

--- newMain.js
+++ newMain.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-    let numTrue = 0;
-    let numFalse = 0;
+    //let numTrue = 0;
+    //let numFalse = 0;
+    let dogCounter = 0;
+    let catCounter = 0;
+    let fishCounter = 0;
+    let birdCounter = 0;
     let affordableAnimals = new Array();
+    let questionCounter = 0;
+    let questions = ["1. How much money are you willing to spend to get the pet?",
+                     "2. How much money do you want to spend in total?", 
+                     "3. Do you like to hold your pet?",
+    				 "4: Do you want a stationary pet?",
+    				 "5: Are you willing to take your pet with you outside?",
+    				 "6: Do you seek an independent pet?",
+    				 "7: Do you want a higher level of maitenence for your pet?",
+    				 "8: Do you want a silent pet?",
+                     "9: Do you want to develop a relationship with your pet?"];
     let animals = [
         {
             type: fish,
@@ -34,23 +48,40 @@ $(document).ready(function() {
             }
         });
     });
-    let questionCounter = 0;
-    let questions = ["1. How much money are you willing to spend to get the pet?",
-                     "2. How much money do you want to spend in total?", 
-                     "3. Do you like to hold your pet?",
-    				 "4: Do you want a stationary pet?",
-    				 "5: Are you willing to take your pet with you outside?",
-    				 "6: Do you seek an independent pet?",
-    				 "7: Do you want a higher level of maitenence for your pet?",
-    				 "8: Do you want a silent pet?",
-    				 "9: Do you want to develop a relationship with your pet?"];
+   
     $(".continue").click(function() {
         let val = $(this).data("value");
         if (val == true) {
-            numTrue++;
-        } else {
-            numFalse++;
-        }
+            //numTrue++;
+            if (questionCounter == 2) {
+                dogCounter++;
+                catCounter++;
+            } 
+            if (questionCounter == 3) {
+                birdCounter++;
+                fishCounter++;
+            } 
+            if (questionCounter == 4) {
+                dogCounter++;
+            }
+             if (questionCounter == 5) {
+                catCounter++;
+                birdCounter++;
+            }
+             if (questionCounter == 6) {
+                dogCounter++;
+                catCounter++;
+            } 
+            if (questionCounter == 7) {
+                catCounter++;
+                fishCounter++;
+            }
+            if (questionCounter == 8) {
+                dogCounter++;
+                catCounter++;
+                birdCounter++;
+            }
+        } 
         questionCounter++;
         $("h2").text(questions[questionCounter]);
     });
