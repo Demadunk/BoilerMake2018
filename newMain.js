f76bb6bc3fad99b6a5a9c189e678ff8b9b281654
lu656 <lu656@purdue.edu>
2018-10-20 09:25:48 -0400

    added start of slider price range stuff

--- newMain.js
+++ newMain.js
@@ -1,8 +1,42 @@
 $(document).ready(function() {
     let numTrue = 0;
     let numFalse = 0;
+    let affordableAnimals = new Array();
+    let animals = [
+        {
+            type: fish,
+            cost: 20
+        },
+        {
+            type: cat,
+            cost: 50
+        },
+        {
+            type: dog,
+            cost: 500
+        },
+        {
+            type: bird,
+            cost: 160
+        }
+    ];
+    $("#pet-price-range").slider({
+        values: [10, 200],
+        max: 500,
+        min: 1,
+        range: true,
+        step: 1,
+    });
+    $("#pet-price-range").change(function(){
+        affordableAnimals = animals.filter(function(animals) {
+            if (animals.cost <= $("#pet-price-range").slider("values[1]")) {
+                return animals;
+            }
+        });
+    });
     let questionCounter = 0;
-    let questions = ["1. question 1", "2. question 2", "3. question 3"];
+    let questions = ["1. How much money are you willing to spend to get the pet?",
+                     "2. How much money do you want to spend in total?", "3. question 3"];
     $(".continue").click(function() {
         let val = $(this).data("value");
         if (val == true) {
